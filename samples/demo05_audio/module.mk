e8df5156a5a225cd277dfd97a71176ce2d948bb9
Jon Mayo <jon@rm-f.net>
2026-04-14 10:55:24 -0700

    demo05_audio: disable closure compiler for Emscripten build
    Miniaudio's EM_JS code uses window.miniaudio which closure compiler's
    ADVANCED_OPTIMIZATIONS mangles, breaking the audio demo. Override the
    global --closure 1 with --closure 0 for this target.

--- samples/demo05_audio/module.mk
+++ samples/demo05_audio/module.mk
@@ -2,4 +2,5 @@ EXECUTABLES += demo05_audio
 demo05_audio_DIR  := $(dir $(lastword $(MAKEFILE_LIST)))
 demo05_audio_SRCS  = demo05_audio.c
 demo05_audio_LIBS = ludica
+demo05_audio_LDFLAGS.Emscripten = --closure 0
 demo05_audio_LDLIBS.Emscripten = -sEVAL_CTORS
