8842c019971687b9c90aab4698be01b465dac509
Jon Mayo <jon@rm-f.net>
2026-04-14 14:04:09 -0700

    lilpc: export _malloc and _free for Emscripten build
    The --pre-js floppy disk UI calls Module._malloc() and Module._free()
    to pass disk image data to the WASM heap. Newer Emscripten requires
    these to be explicitly listed in EXPORTED_FUNCTIONS.

--- samples/lilpc/module.mk
+++ samples/lilpc/module.mk
@@ -2,5 +2,5 @@ EXECUTABLES += lilpc
 lilpc_DIR  := $(dir $(lastword $(MAKEFILE_LIST)))
 lilpc_SRCS  = lilpc.c cpu286.c bus.c chipset.c video.c kbd.c fdc.c serial.c parallel.c speaker.c debugmon.c
 lilpc_LIBS = ludica
-lilpc_LDLIBS.Emscripten = -sEVAL_CTORS -sEXPORTED_RUNTIME_METHODS=ccall
+lilpc_LDLIBS.Emscripten = -sEVAL_CTORS -sEXPORTED_RUNTIME_METHODS=ccall -sEXPORTED_FUNCTIONS=_main,_malloc,_free
 lilpc_LDFLAGS.Emscripten = --preload-file $(lilpc_DIR)bios/pcxtbios.bin@bios/pcxtbios.bin --pre-js $(lilpc_DIR)emscripten_args.js
