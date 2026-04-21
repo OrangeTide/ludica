014e9fd651d499c8f31ba9710d73b44e8608fff0
Jon Mayo <jon@rm-f.net>
2026-04-21 01:28:39 -0700

    vendor Khronos EGL/GLES/KHR headers so CI release can package them
    The headers were gitignored and only existed locally, causing the
    release workflow to fail with 'cannot stat src/ludica/include/EGL'.
    
    Also: move download-headers.sh to scripts/, update it to work from
    repo root, and simplify release.yml header copying (glob + fix
    missing ludica_anim.h, ludica_audio.h, ludica_auto.h, ludica_slug.h).

--- scripts/download-headers.sh
+++ scripts/download-headers.sh
@@ -16,26 +16,28 @@ set -- \
 "https://registry.khronos.org/EGL/api/EGL/eglplatform.h" \
 "https://registry.khronos.org/EGL/api/KHR/khrplatform.h"
 
+INCDIR="src/ludica/include"
+
 for url in "$@" ; do
   B="${url##*://*/api/}"
-  if [ ! -e "include/$B" ]; then
+  if [ ! -e "${INCDIR}/$B" ]; then
     echo "Downloading $url ..."
-    curl -sS -L --create-dirs --output "include/$B" "$url"
+    curl -sS -L --create-dirs --output "${INCDIR}/$B" "$url"
   else
     echo "[SKIP] Downloading $url ..."
   fi
 done
 
 cat <<EOF | sha1sum -c
-108a22ccb13ea471940804584e01fbefa78a4872  include/EGL/eglext.h
-30b8964110af967b8ca07ef4530f56a88d849c21  include/EGL/egl.h
-60963b845b8d7570520c853f5e0a36a645fd3cbc  include/EGL/eglplatform.h
-0e8f5f850f65c859faf561f8495a83ecd13b7030  include/GLES2/gl2ext.h
-8243af14859460eb33bfc933687dc8388fab0d3b  include/GLES2/gl2.h
-39b2372c3d254addee407682f44760ca22116c7e  include/GLES2/gl2platform.h
-eb90411ed714acc684c225e1ae7d3105e6a0a9e3  include/GLES3/gl31.h
-44cc0f878a3eef929fbc576ff7b9e614e005a351  include/GLES3/gl32.h
-00a12f93da92c9d0d1dfd0842c7fcfc070773404  include/GLES3/gl3.h
-a032fbbd07a90fd378a154834d3dc6ce93ee198d  include/GLES3/gl3platform.h
-9818ea12cfb96c60bdbb5103d8cf7754dcce34f4  include/KHR/khrplatform.h
+108a22ccb13ea471940804584e01fbefa78a4872  ${INCDIR}/EGL/eglext.h
+30b8964110af967b8ca07ef4530f56a88d849c21  ${INCDIR}/EGL/egl.h
+60963b845b8d7570520c853f5e0a36a645fd3cbc  ${INCDIR}/EGL/eglplatform.h
+0e8f5f850f65c859faf561f8495a83ecd13b7030  ${INCDIR}/GLES2/gl2ext.h
+8243af14859460eb33bfc933687dc8388fab0d3b  ${INCDIR}/GLES2/gl2.h
+39b2372c3d254addee407682f44760ca22116c7e  ${INCDIR}/GLES2/gl2platform.h
+eb90411ed714acc684c225e1ae7d3105e6a0a9e3  ${INCDIR}/GLES3/gl31.h
+44cc0f878a3eef929fbc576ff7b9e614e005a351  ${INCDIR}/GLES3/gl32.h
+00a12f93da92c9d0d1dfd0842c7fcfc070773404  ${INCDIR}/GLES3/gl3.h
+a032fbbd07a90fd378a154834d3dc6ce93ee198d  ${INCDIR}/GLES3/gl3platform.h
+9818ea12cfb96c60bdbb5103d8cf7754dcce34f4  ${INCDIR}/KHR/khrplatform.h
 EOF
